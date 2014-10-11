729c048955802120c20bc1096bc00d8db7828ffa
hughes <hughes@goemon.home>
2014-10-11 16:28:56 -0400

    Updated README.md + few changes hanoi.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 hanoi
 =====
+
+Hanoi towers: basic game, with demo on how to solve it (recursively)
--- hanoi.py
+++ hanoi.py
@@ -1,9 +1,24 @@
-n = 3
+n = 4
 array = [ [i for i in xrange(1, n+1)], [0 for i in xrange(n)], [0 for i in xrange(n)] ]
-empty = "     |     "
-disk1 = "    _|_    "
-disk2 = "   __|__   "
-disk3 = "  ___|___  "
+disque = [ "" for i in xrange(n+1) ]
+
+def initialize():
+	for i in xrange(n+1):
+		disque[i] += " " * (n+2-i)
+		disque[i] += "_" * i
+		disque[i] += "|"
+		disque[i] += "_" * i
+		disque[i] += " " * (n+2-i)
+
+def main():
+	initialize()
+	demo(n, 0, 2, 1)
+"""	while (1):
+		display()
+	 	start = int(raw_input("From: "))
+	 	end = int(raw_input("To: "))
+	 	if licit(start, end):
+			move(start, end)"""
 
 def licit(start, end):
 	return True
@@ -47,33 +62,14 @@ def demo(n, start, end, inter):
 	move(start, end)
 	demo(n-1, inter, end, other(end, inter))
 		
-
-def main():
-	demo(n, 0, 2, 1)
-"""	while (1):
-		display()
-	 	start = int(raw_input("From: "))
-	 	end = int(raw_input("To: "))
-	 	if licit(start, end):
-			move(start, end)"""
-
-def disk(i):
-	if i==1:
-		return disk1
-	if i==2:
-		return disk2
-	if i==3:
-		return disk3
-	return empty
-
 def single_line(i):
-	column1 = disk(array[0][i])
-	column2 = disk(array[1][i])
-	column3 = disk(array[2][i])
+	column1 = disque[array[0][i]]
+	column2 = disque[array[1][i]]
+	column3 = disque[array[2][i]]
 	print column1, column2, column3
 
 def display():
-	for i in xrange(3):
+	for i in xrange(n):
 		single_line(i)
-	bottom_line()
+	print disque[0]
 
